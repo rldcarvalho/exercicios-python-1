97e8567dd6759f4099d7e392e4a94b76f0f140b7
Rafael Carvalho <rafa_rafael_lucas@hotmail.com>
2022-05-12 15:35:48 -0300

    Aprimoramento do código - adivinhacao
    Substituido o laço while por for.
    Limitado o range do chute inicial do usuário.

--- advinhacao.py
+++ advinhacao.py
@@ -6,13 +6,16 @@ numero_secreto = 42
 total_de_tentativas = 3
 rodada = 1
 
-while(rodada <= total_de_tentativas):
+for rodada in range(0,total_de_tentativas):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute_str = input("Digite seu número: ")
+    chute_str = input("Digite um número entre um e 100: ")
 
     print("Você digitou ", chute_str)
-
     chute = int(chute_str)
+
+    if chute < 1 or chute > 100:
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
@@ -21,12 +24,11 @@ while(rodada <= total_de_tentativas):
     if (acertou):
         print("Você acertou!")
         rodada = total_de_tentativas + 1
+        break
     else:
         if(maior):
             print("Você errou! O seu chute foi maior que o número secreto")
-            rodada = rodada + 1
         elif (menor):
             print("Você errou! O seu chute foi menor que o número secreto")
-            rodada = rodada + 1
 
 print("Fim do jogo")
