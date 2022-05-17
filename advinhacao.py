9bfea5181e7c0a15d7c62f407e9b425fdcb6eef9
Rafael Carvalho <rafa_rafael_lucas@hotmail.com>
2022-05-17 18:35:57 -0300

    Adição de funcionalidades - adivinhacao
    Implementada função para definir o nível de dificuldade do jogo. Foi utilizado arrays com associação de índices ao invés de obter o numero de tentativas por "ifs";
    Implementação do sistema de pontuação para o jogo;
    Pequenas modificações para melhorar a legibilidade do código.

--- advinhacao.py
+++ advinhacao.py
@@ -4,13 +4,29 @@ print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
-total_de_tentativas = 3
-
 numero_secreto = random.randrange(1, 101)
+pontuacao = 1000
 
-for rodada in range(1,total_de_tentativas + 1):
+
+def selecionar_nivel():
+    tab_dificuldade = [1, 2, 3]
+    tab_tentativas = [20, 10, 5]
+    print("Qual o nível de dificuldade?")
+    print("(1) Fácil (2) Médio (3) Difícil")
+    nivel = int(input("Defina o nível: "))
+    if nivel < 1 or nivel > 3:
+        print("Número inválido")
+        nivel = int(input("Defina o nível com um número de 1 a 3!"))
+    tentativas = tab_dificuldade.index(nivel)
+    return tab_tentativas[tentativas]
+
+
+total_de_tentativas = selecionar_nivel()
+
+
+for rodada in range(1, total_de_tentativas + 1):
     print(f'Tentativa {rodada} de {total_de_tentativas}')
-    chute_str = input("Digite um número entre um e 100: ")
+    chute_str = input("Digite um número entre 1 e 100: ")
 
     print("Você digitou ", chute_str)
     chute = int(chute_str)
@@ -23,14 +39,17 @@ for rodada in range(1,total_de_tentativas + 1):
     maior = chute > numero_secreto
     menor = chute < numero_secreto
 
-    if (acertou):
+    if not acertou:
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontuacao = pontuacao - pontos_perdidos
+        if maior:
+            print("Você errou! O seu chute foi maior que o número secreto")
+        elif menor:
+            print("Você errou! O seu chute foi menor que o número secreto")
+    else:
         print("Você acertou!")
         rodada = total_de_tentativas + 1
+        print(f'Sua pontuação foi de {pontuacao}.')
         break
-    else:
-        if(maior):
-            print("Você errou! O seu chute foi maior que o número secreto")
-        elif (menor):
-            print("Você errou! O seu chute foi menor que o número secreto")
 
 print("Fim do jogo")
