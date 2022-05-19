f3f996f95911381788018b791a2ea9eaf379842c
Rafael Carvalho <rafa_rafael_lucas@hotmail.com>
2022-05-19 17:35:38 -0300

    Adição de funcionalidades - forca
    Adicionado laço for para percorrer a string buscando a letra chutada no jogo;
    Adicionada list para preencher as letras acertadas pelo jogador;
    Implementadas condições para encerramento do jogo.

--- forca.py
+++ forca.py
@@ -3,13 +3,39 @@ def jogar():
     print("***Bem vindo ao jogo de Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_"] * len(palavra_secreta)
+    #alternativamente: letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
-    acertou = True
+    acertou = False
+    erros = 0
+
+    print(letras_acertadas)
 
     while(not enforcou and not acertou):
-        
+
+        chute = input("Qual a letra? ")
+        chute = chute.strip().upper()
+
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+            print(f"Ops, você errou! Faltam {6-erros} tentativas.")
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+    if acertou:
+        print("Você ganhou!")
+    else:
+        print("Você perdeu")
 
     print("Fim do jogo")
 
